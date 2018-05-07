b69bf60dfed1d69a8802e055f078e137d1841de9
Shpiler7 <pcts79@gmail.com>
2018-05-07 14:52:47 +0200

    optimization

--- logic/Game.py
+++ logic/Game.py
@@ -1,11 +1,12 @@
 import random
+import numpy as np
 
 class Game(object):
 
     def __init__(self):
-        self.size = 10
+        self.size = 19
         self.winNumber = 5
-        self.board = [['-' for x in range(self.size)] for y in range(self.size)]
+        self.board = np.array([['-' for x in range(self.size)] for y in range(self.size)])
 
     def start(self):
         sign = random.choice(['X', 'O'])
@@ -15,7 +16,7 @@ class Game(object):
             place = place.split(' ')
             if 1 <= int(place[0]) <= self.size and 1 <= int(place[1]) <= self.size and self.board[int(place[0])-1][int(place[1])-1] == '-':
                 self.board[int(place[0])-1][int(place[1])-1] = sign
-                if self.check_board():
+                if self.check_win(int(place[0])-1, int(place[1])-1, sign):
                     print(sign + " wins")
                     self.draw_board()
                     break
@@ -35,43 +36,80 @@ class Game(object):
                 print(self.board[x][y], end=" ")
             print()
 
-    def check_board(self):
+    def check_win(self, x, y, sign):
 
-        for i in range(self.size):
-            for j in range(0, self.size - self.winNumber):
-                counter = 0
-                for k in range(self.winNumber):
-                    if self.board[i][j] != '-' and self.board[i][j] == self.board[i][j+k]:
-                        counter = counter + 1
-                if counter >= self.winNumber:
-                    return True
+        i = x - 1
+        lcounter = 0
+        rcounter = 0
+        while self.board[i][y] == sign:
+            lcounter = lcounter + 1
+            i = i - 1
 
-        for i in range(self.size):
-            for j in range(0, self.size - self.winNumber):
-                counter = 0
-                for k in range(self.winNumber):
-                    if self.board[j][i] != '-' and self.board[j][i] == self.board[j+k][i]:
-                        counter = counter + 1
-                    if counter >= self.winNumber:
-                        return True
+        i = x + 1
+        while i < self.size and self.board[i][y] == sign:
+            rcounter = rcounter + 1
+            i = i + 1
 
-        for i in range(self.size - self.winNumber + 1):
-            for j in range(self.size - self.winNumber + 1):
-                counter = 0
-                for k in range(self.winNumber):
-                    if self.board[i][j] != '-' and self.board[i][j] == self.board[i + k][j + k]:
-                        counter = counter + 1
-                    if counter >= self.winNumber:
-                        return True
+        counter = lcounter + rcounter + 1
+        if counter == self.winNumber:
+            return True
 
-        for i in range(self.size - self.winNumber):
-            for j in range(self.winNumber - 1, self.size):
-                counter = 0
-                for k in range(self.winNumber):
-                    if self.board[i][j] != '-' and self.board[i][j] == self.board[i + k][j - k]:
-                        counter = counter + 1
-                    if counter >= self.winNumber:
-                        return True
+        j = y - 1
+        upcounter = 0
+        downcounter = 0
+        while self.board[x][j] == sign:
+            downcounter = downcounter + 1
+            j = j - 1
+
+        j = y + 1
+        while j < self.size and self.board[x][j] == sign:
+            upcounter = upcounter + 1
+            j = j + 1
+
+        counter = upcounter + downcounter + 1
+        if counter == self.winNumber:
+            return True
+
+        i = x + 1
+        j = y + 1
+        rdowncounter = 0
+        lupcounter = 0
+        while i < self.size and j < self.size and self.board[i][j] == sign:
+            rdowncounter = rdowncounter + 1
+            j = j + 1
+            i = i + 1
+
+        i = x - 1
+        j = y - 1
+        while j < self.size and self.board[i][j] == sign:
+            lupcounter = lupcounter + 1
+            j = j - 1
+            i = i - 1
+
+        counter = lupcounter + rdowncounter + 1
+        if counter == self.winNumber:
+            return True
+
+        i = x + 1
+        j = y - 1
+        rupcounter = 0
+        ldowncounter = 0
+        while i < self.size and self.board[i][j] == sign:
+            rupcounter = rupcounter + 1
+            j = j - 1
+            i = i + 1
+
+        i = x - 1
+        j = y + 1
+        while j < self.size and self.board[i][j] == sign:
+            ldowncounter = ldowncounter + 1
+            j = j + 1
+            i = i - 1
+
+        counter = ldowncounter + rupcounter + 1
+        if counter == self.winNumber:
+            return True
+
         return False
 
     def is_board_full(self):
